3117c9f3d9db27b4cde74b3cc4b0c33bde72a15c
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-11-18 10:19:24 +0900

    エラー表示の実装

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateTodoRequest;
 use App\Todo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -38,15 +39,15 @@ class TodoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateTodoRequest $request)
     {
         $todo = new todo();
         $todo->title = $request->title;
         $todo->due_date = $request->due_date;
         $todo->status = Todo::STATUS_NOT_YET;
 
-        Auth()::user()->todos()->save($todo);
-        return view('welcom');
+        Auth::user()->todos()->save($todo);
+        return redirect()->to('/todo');
     }
 
     /**
