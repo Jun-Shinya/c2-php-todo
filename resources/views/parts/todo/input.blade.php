73367879c6fd8b3926c0824bd1fe444c100edad1
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-11-25 09:45:04 +0900

    タスク編集画面の実装

--- resources/views/parts/todo/input.blade.php
+++ resources/views/parts/todo/input.blade.php
@@ -1,10 +1,10 @@
 <div class="form-group">
     <label for="title">タスク名</label>
-    <input type="text" class="form-control" name="title" placeholder="ゴミ出し" required value="{{ $todo->title }}">
+    <input type="text" class="form-control" name="title" placeholder="ゴミ出し" value="{{ $todo->title ? $todo->title: ''}}" required>
 </div>
 <div class="form-group">
     <label for="due_date">期限</label>
-    <input type="date" class="form-control" name="due_date" placeholder="2020/10/31" required value="{{ $todo->due_date }}">
+    <input type="date" class="form-control" name="due_date" placeholder="2020/10/31" value="{{ $todo->due_date ? $todo->due_date: '' }}" required>
 </div>
 <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
 @if ($errors->any())
--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -4,9 +4,9 @@
     <div class="container">
         <div class="col-md-10">
             <h2 class="text-muted py-3">Todo 編集</h2>
-            <form action="/todo" method="POST">
-                @include('parts.todo.input')
-                <button class="btn btn-primary" type="submit">更新</button>
+            <form action="/todo/{{ $todo->id }}" method="POST">
+                @include('parts.todo.input', ['todo' => $todo])
+                <button class="btn btn-primary my-2" type="submit">更新</button>
             </form>
             <hr>
             @include('parts.button.back')
